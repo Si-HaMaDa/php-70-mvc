d2d16e12ea53fa4db4233a8bf2c247a4aa19c7d5
Mohamed <nrookman_27@yahoo.com>
2022-06-13 19:43:39 +0200

    made router

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('DB_TYPE', "mysql");
+define('DB_HOST', "localhost");
+define('DB_USER_NAME', "root");
+define('DB_PASSWORD', "root");
+define('DB_NAME', "php70");
+
+
+define('MAIN_DOMAIN', 'http://localhost');
+
+define('SUB_FOLDER', '/php-70-mvc');
+
+define('FULL_URL', MAIN_DOMAIN . SUB_FOLDER);
--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <?php
 
-$request = $_GET['route'] ?? '/';
-
-// die($request);
+require __DIR__ . '/config.php';
+// Get the method name from the URL
+$route = $_SERVER['REQUEST_URI'];
+// Replace the Subfolder with an empty string
+$route = str_replace(SUB_FOLDER, '', $route);
+// Remove query params
+$route = explode('?', $route)[0];
+// Remove the last slash
+$route = rtrim($route, '/');
 
 require './controllers/UserController.php';
 
-$userController = new UserController();
-
-switch ($request) {
-    case '/':
-        $userController->index();
+switch ($route) {
+    case '/admin/users':
+        (new UserController())->index();
         break;
-    case 'add':
-        $userController->add();
+    case '/admin/users/add':
+        (new UserController())->add();
         break;
-    case 'show':
-        $userController->show();
+    case '/admin/users/show':
+        (new UserController())->show();
         break;
     default:
         http_response_code(404);
